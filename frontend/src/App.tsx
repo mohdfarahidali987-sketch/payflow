a4617814d8dbbb3a1c4fd61c2413cc96c5576cda
Farahid <mohdfarahidali987@gmail.com>
2026-08-09 15:16:13 +0530

    Add default route to signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Dashboard } from "./pages/Dashboard";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import { SendMoney } from "./pages/SendMoney";
 import { Signin } from "./pages/Signin";
 import { Signup } from "./pages/Signup";
@@ -11,6 +11,7 @@ function App() {
   <Toaster position="top-right" />
     <BrowserRouter>
       <Routes>
+         <Route path="/" element={<Navigate to="/signin" replace />} />
     <Route path="/signup" element={<Signup />} />
 
     <Route path="/signin" element={<Signin />} />
